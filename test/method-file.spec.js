58f301b5802dce163405d81216bae0faffe7ef38
Erikaquispenjanampa <erikaquispejanampa@gmail.com>
2020-05-25 03:43:43 -0500

    Testeo de funcionalidad geFileDirectory

--- test/method-file.spec.js
+++ test/method-file.spec.js
@@ -2,6 +2,16 @@ const file = require("../util/method-file");
 
 let contentFile = 'prueba de contenido';
 
+let DirectoryFiles = [
+  'E:\\Laboratoria\\Proyectos\\LIM012-fe-md-links\\MDs\\contenido.md',
+  'E:\\Laboratoria\\Proyectos\\LIM012-fe-md-links\\MDs\\prueba1.md',  
+  'E:\\Laboratoria\\Proyectos\\LIM012-fe-md-links\\MDs\\prueba2.md',  
+  'E:\\Laboratoria\\Proyectos\\LIM012-fe-md-links\\MDs\\prueba3.md',
+  'E:\\Laboratoria\\Proyectos\\LIM012-fe-md-links\\MDs\\prueba4.txt'
+];
+
+
+
 describe('Probar si es objeto Funcion', () => {
     it('funcion typeFile', () => {
       expect(typeof file.typeFile).toBe('function');
@@ -30,6 +40,10 @@ describe('Probar valores', () => {
     
     it('Validar contenido de archivo', () => {
       expect(file.readFile('E:\\Laboratoria\\Proyectos\\LIM012-fe-md-links\\MDs\\contenido.md')).toBe(contentFile);
+    });
+
+    it('Validar archivos del directorio', () => {
+      expect(file.getFileDirectory('E:\\Laboratoria\\Proyectos\\LIM012-fe-md-links\\MDs\\').length).toBe(DirectoryFiles.length);
     });
 
 })
